2f82cd9f593478a82b0405e37d9b8201cf5c6439
nagarajababu <nagaraja.ev@gmail.com>
2022-03-14 04:02:54 +0530

    Login testcase

--- test_script/test_valid_login.py
+++ test_script/test_valid_login.py
@@ -1,15 +1,21 @@
 from generic.base_test import BaseTest
+from generic.excel import Excel
 from page.login_page import Login_Page
 from page.dashboard_page import Dashboard_Page
 class Test_Valid_Login(BaseTest):
 
     def test_valid_login(self):
-# 1. enter valied username
+
+#get data from Excel
+        un=Excel.get_dat(self.xl_path,"ValidLogin",2,1)
+        pw=Excel.get_dat(self.xl_path,"ValidLogin",2,2)
+
+        # 1. enter valied username
         login_page=Login_Page(self.driver)
-        login_page.set_username("Admin")
+        login_page.set_username(un)
 
 # 2. enter valid password
-        login_page.set_password("admin123")
+        login_page.set_password(pw)
 
 # 3. click login button
         login_page.click_login_button()
